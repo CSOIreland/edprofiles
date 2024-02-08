eb5038644ae70add3ec90c100f0eaac9ef4c74bb
tomaskellycso <158488395+tomaskellycso@users.noreply.github.com>
2024-02-08 11:58:40 +0000

    New Search, New Index, Readme Edits, New example reports

--- 2022/health/rcode/HealthProfileMain.R
+++ 2022/health/rcode/HealthProfileMain.R
@@ -13,8 +13,9 @@ library(stringi)
 TodaysDate <- format(Sys.Date(), "%d/%m/%Y")
 TodayForJSONLD <- format(Sys.Date(), "%Y/%m/%d/")
 
-#set up working directory, input and output folders
-RootWD <- setwd(getwd())
+#set up working directory, input and output folders. 
+RootWD <- getwd()
+setwd(RootWD)
 InputFilesLoc <- file.path(RootWD, "inputs")
 OutputFilesLoc <- file.path(RootWD, "outputs")
 
@@ -72,20 +73,20 @@ EDWGUIDAC$ACLC <- gsub(".","-",EDWGUIDAC$ACLC, fixed = T)
 EDWGUIDAC$GUIDLC <- tolower(EDWGUIDAC$GUID)
 
 #Create CSV For HTML Table and EDsearch tool with links to each file
-CSVForHTML <- EDWGUIDAC%>%select(ED,GUID,AC,EDLC,ACLC)
+CSVForHTML <- EDWGUIDAC%>%select(ED,GUID,AC,EDLC,ACLC,GUIDLC)
 CSVForHTML$ReportNumber <- 1:nrow(CSVForHTML)
 
 #add links for html and pdf reports
 CSVForHTML$Report <- paste0("<a href=\".\\reports\\html\\",CSVForHTML$ReportNumber,"--csohealthprofile--edname--",CSVForHTML$EDLC,"--acname--",CSVForHTML$ACLC,"--edguid--",CSVForHTML$GUIDLC,".html","\">","HTML","</a>", "  ",
-                                "<a href=\".\\reports\\pdf\\",CSVForHTML$ReportNumber,"-csohealthprofile--edname--",CSVForHTML$EDLC,"--acname--",CSVForHTML$ACLC,"--edguid--",CSVForHTML$GUIDLC,".pdf","\">","PDF","</a>"  )
+                            "<a href=\".\\reports\\pdf\\",CSVForHTML$ReportNumber,"--csohealthprofile--edname--",CSVForHTML$EDLC,"--acname--",CSVForHTML$ACLC,"--edguid--",CSVForHTML$GUIDLC,".pdf","\">","PDF","</a>"  )
 
 CSVForHTML <- CSVForHTML%>%select(ED,AC,Report, GUID)
 
 write.csv(CSVForHTML, file = paste0(OutputFilesLoc, "/CSVForHTML.csv"), row.names = F)
 
 # #sample dataset if running tests
-Sample <- sample(1:nrow(EDWGUIDAC),2)
-EDWGUIDAC <- EDWGUIDAC[Sample,]
+# Sample <- sample(1:nrow(EDWGUIDAC),2)
+# EDWGUIDAC <- EDWGUIDAC[Sample,]
 
 
 # create an empty list for errors, to be filled later
@@ -93,89 +94,89 @@ ErrorList <- list()
 
 # Loop through every file in the process for every ED
 for (i in 1:nrow(EDWGUIDAC))  {
-setwd(RootWD)
-
-#skip to next controls whether an iteration should be skipped if there is an error(if SkipToNext == TRUE)
-SkipToNext <- FALSE
-
-tryCatch({
-#Select the appropriate, ED, AC and map link
-ED <- as.character(EDWGUIDAC$ED[i])
-EDGUID <- as.character(EDWGUIDAC$GUID[i])
-EDGUIDLC <- as.character(EDWGUIDAC$GUIDLC[i])
-EDLC <- as.character(EDWGUIDAC$EDLC[i])
-
-AC <- EDWGUIDAC$AC[i]
-ACGUID <- ACList$GUID[ACList$Administrative.Counties.2019 == AC]
-ACLC <- as.character(EDWGUIDAC$ACLC[i])
-
-#Create a truncated version of ED so that tables and plots remain consistent
-EDForTables <- str_trunc(ED, 25)
-EDForTables <- str_replace_all(EDForTables, "[^[:alnum:]]\\,", " ")
-EDName <- make.names(ED)
-EDName <- gsub("\\.","-", EDName)
-
-
-#Create a truncated version of AC so that tables and plots remain consistent
-ACForTables <- str_trunc(AC, 25)
-ACForTables <- str_replace_all(ACForTables, "[^[:alnum:]]\\,", " ")
-ACForTablesShort <- str_trunc(AC, 15)
-ACForTablesShort <- str_replace_all(ACForTablesShort , "[^[:alnum:]]\\,", " ")
-ACName <- make.names(AC)
-ACName <-  gsub("\\.","-", ACName)
-
-# Title for the markdown/html
-
-TitleForRMD <- paste0("Electoral Division Health Profile - ", ED)
-
-# link to map for ED
-EDMapLink <- paste0(InputFilesLoc,"/exportededmaps/",EDGUID,".jpg")
-
-# Run through Subfiles
-source(paste0(getwd(),"/scripts/1_Population.R"))
-source(paste0(getwd(),"/scripts/2_Carers.R"))
-source(paste0(getwd(),"/scripts/3_GeneralHealth.R"))
-source(paste0(getwd(),"/scripts/4_Disability.R"))
-source(paste0(getwd(),"/scripts/5_Smoking.R"))
-source(paste0(getwd(),"/scripts/6_Education.R"))
-source(paste0(getwd(),"/scripts/7_PrincipleEconomicStatus.R"))
-source(paste0(getwd(),"/scripts/8_Families.R"))
-source(paste0(getwd(),"/scripts/9_Birthplace.R"))
-source(paste0(getwd(),"/scripts/11_Volunteering.R"))
-source(paste0(getwd(),"/scripts/12_SocialClass.R"))
-source(paste0(getwd(),"/scripts/13_Households.R"))
-source(paste0(getwd(),"/scripts/14_RenewableEnergy.R"))
-source(paste0(getwd(),"/scripts/15_Travel.R"))
-source(paste0(getwd(),"/scripts/16_KeyPointsAndOtherCalcs.R"))
-source(paste0(getwd(),"/scripts/17_SAPS.R"))
-
-setwd(paste0(getwd(),"/scripts"))
-
-#Create the .RNW file using sweave for compiling
-Sweave("HealthProfileTemplate.Rnw",output=paste0(i,"--csohealthprofile--edname--",EDLC,"--acname--",ACLC,"--edguid--",EDGUIDLC,".tex"))
-
-#Compile the .rnw with Latex
-tools::texi2pdf(paste0(i,"--csohealthprofile--edname--",EDLC,"--acname--",ACLC,"--edguid--",EDGUIDLC,".tex"))
-tools::texi2pdf(paste0(i,"--csohealthprofile--edname--",EDLC,"--acname--",ACLC,"--edguid--",EDGUIDLC,".tex"))
-
-#EDProfile pdf Link for RMD
-EDLinkPDF<- paste0("<font size=\"5\"><a href=\".\\profiles\\pdf\\",i,"-csohealthprofile--edname--",EDLC,"--acname--",ACLC,"--edguid--",EDGUIDLC,".pdf\""," style=\"text-decoration: none\">A more detailed and print friendly pdf profile - with accompanying tables - is available here.</a></font>")
-
-#render R Markdown  
-rmarkdown::render("HealthProfileMarkdown.Rmd", output_file =paste0(i,"--csohealthprofile--edname--",EDLC,"--acname--",ACLC,"--edguid--",EDGUIDLC,".html"))
-
-
-# print progress
-print(paste0("Iteration ", i, " of ", nrow(EDWGUIDAC), " complete(",round(i*100/nrow(EDWGUIDAC),1),"%)"))
-
-# log errors
-}, error = function(e) {
-  ErrorMessage <- paste0("Error in Iteration ", i ,": for ED -",ED,"- ", conditionMessage(e))
-  ErrorList <<- c(ErrorList,ErrorMessage)
-  SkipToNext <<-TRUE
-}
-)
-#if SkipToNext is TRUE, skip to next iteration
+  setwd(RootWD)
+  
+  #skip to next controls whether an iteration should be skipped if there is an error(if SkipToNext == TRUE)
+  SkipToNext <- FALSE
+  
+  tryCatch({
+    #Select the appropriate, ED, AC and map link
+    ED <- as.character(EDWGUIDAC$ED[i])
+    EDGUID <- as.character(EDWGUIDAC$GUID[i])
+    EDGUIDLC <- as.character(EDWGUIDAC$GUIDLC[i])
+    EDLC <- as.character(EDWGUIDAC$EDLC[i])
+    
+    AC <- EDWGUIDAC$AC[i]
+    ACGUID <- ACList$GUID[ACList$Administrative.Counties.2019 == AC]
+    ACLC <- as.character(EDWGUIDAC$ACLC[i])
+    
+    #Create a truncated version of ED so that tables and plots remain consistent
+    EDForTables <- str_trunc(ED, 25)
+    EDForTables <- str_replace_all(EDForTables, "[^[:alnum:]]\\,", " ")
+    EDName <- make.names(ED)
+    EDName <- gsub("\\.","-", EDName)
+    
+    
+    #Create a truncated version of AC so that tables and plots remain consistent
+    ACForTables <- str_trunc(AC, 25)
+    ACForTables <- str_replace_all(ACForTables, "[^[:alnum:]]\\,", " ")
+    ACForTablesShort <- str_trunc(AC, 15)
+    ACForTablesShort <- str_replace_all(ACForTablesShort , "[^[:alnum:]]\\,", " ")
+    ACName <- make.names(AC)
+    ACName <-  gsub("\\.","-", ACName)
+    
+    # Title for the markdown/html
+    
+    TitleForRMD <- paste0("Electoral Division Health Profile - ", ED)
+    
+    # link to map for ED
+    EDMapLink <- paste0("../inputs/exportededmaps/",EDGUID,".jpg")
+    
+    # Run through Subfiles
+    source(paste0(getwd(),"/scripts/1_Population.R"))
+    source(paste0(getwd(),"/scripts/2_Carers.R"))
+    source(paste0(getwd(),"/scripts/3_GeneralHealth.R"))
+    source(paste0(getwd(),"/scripts/4_Disability.R"))
+    source(paste0(getwd(),"/scripts/5_Smoking.R"))
+    source(paste0(getwd(),"/scripts/6_Education.R"))
+    source(paste0(getwd(),"/scripts/7_PrincipleEconomicStatus.R"))
+    source(paste0(getwd(),"/scripts/8_Families.R"))
+    source(paste0(getwd(),"/scripts/9_Birthplace.R"))
+    source(paste0(getwd(),"/scripts/11_Volunteering.R"))
+    source(paste0(getwd(),"/scripts/12_SocialClass.R"))
+    source(paste0(getwd(),"/scripts/13_Households.R"))
+    source(paste0(getwd(),"/scripts/14_RenewableEnergy.R"))
+    source(paste0(getwd(),"/scripts/15_Travel.R"))
+    source(paste0(getwd(),"/scripts/16_KeyPointsAndOtherCalcs.R"))
+    source(paste0(getwd(),"/scripts/17_SAPS.R"))
+    
+    setwd(paste0(getwd(),"/scripts"))
+    
+    #Create the .RNW file using sweave for compiling
+    Sweave("HealthProfileTemplate.Rnw",output=paste0(i,"--csohealthprofile--edname--",EDLC,"--acname--",ACLC,"--edguid--",EDGUIDLC,".tex"))
+    
+    #Compile the .rnw with Latex
+    tools::texi2pdf(paste0(i,"--csohealthprofile--edname--",EDLC,"--acname--",ACLC,"--edguid--",EDGUIDLC,".tex"))
+    tools::texi2pdf(paste0(i,"--csohealthprofile--edname--",EDLC,"--acname--",ACLC,"--edguid--",EDGUIDLC,".tex"))
+    
+    #EDProfile pdf Link for RMD
+    EDLinkPDF<- paste0("<font size=\"5\"><a href=\"..\\pdf\\",i,"--csohealthprofile--edname--",EDLC,"--acname--",ACLC,"--edguid--",EDGUIDLC,".pdf\""," style=\"text-decoration: none\">A more detailed and print friendly pdf profile - with accompanying tables - is available here.</a></font>")
+    
+    #render R Markdown  
+    rmarkdown::render("HealthProfileMarkdown.Rmd", output_file =paste0(i,"--csohealthprofile--edname--",EDLC,"--acname--",ACLC,"--edguid--",EDGUIDLC,".html"))
+    
+    
+    # print progress
+    print(paste0("Iteration ", i, " of ", nrow(EDWGUIDAC), " complete(",round(i*100/nrow(EDWGUIDAC),1),"%)"))
+    
+    # log errors
+  }, error = function(e) {
+    ErrorMessage <- paste0("Error in Iteration ", i ,": for ED -",ED,"- ", conditionMessage(e))
+    ErrorList <<- c(ErrorList,ErrorMessage)
+    SkipToNext <<-TRUE
+  }
+  )
+  #if SkipToNext is TRUE, skip to next iteration
   if(SkipToNext) { next }
 }
 
@@ -186,3 +187,6 @@ print(ErrorList)
 
 #write a csv of errors
 write.csv(as.data.frame(ErrorList), file = paste0(OutputFilesLoc,"/ErrorList.csv"))
+
+#setWD to if rerunning runs correctly
+setwd(RootWD)
--- README.md
+++ README.md
@@ -16,19 +16,20 @@ This work is licensed under a
 
 This project originated as a collaboration between the Central Statistics Office and Cork Healthy Cities in Ireland. The goals were to profile every Electoral Division (ED) from a health perspective, taking into account the social determinants of health (economic status, education, housing etc). 
 
-There are a total of three key webpages for users to visit. The homepage contains a map of Electoral Divisions in Ireland, from which a user can select the appropriate HTML or PDF profile report (HTML reports contain only charts, whereas the PDFs contain data tables in addition to charts.) . From the homepage, the user can navigate to a searchable table to filter reports for a specific ED or Administrative County. Finally, the user can navigate from the homepage to the Github repository containing the code required to generate the reports. 
+The <a href="https://www.edprofiles.cso.ie/2022/health">Homepage here</a> contains a map of Electoral Divisions in Ireland, from which a user can select the appropriate HTML or PDF profile report (HTML reports contain only charts, whereas the PDFs contain data tables in addition to charts.) . From the homepage, the user can navigate to a searchable table to filter reports for a specific ED or Administrative County. Finally, the user can navigate from the homepage to the Github repository containing the code required to generate the reports. 
 
-The code creates PDF and HTML reports for every ED in Ireland using R. It takes a variety of data that is publicly available on http//www.cso.ie, adjusts their structure/formatting appropriately and creates a series of tables and plots that are then included in the final reports. It also creates the searchable map, as well as a csv that is used to create the searchable table contained on the website using the Python package “csvtotable”. 
+The code creates PDF and HTML reports for every ED in Ireland using R. It takes a variety of data that is publicly available on http://www.cso.ie, adjusts their structure/formatting appropriately and creates a series of tables and plots that are then included in the final reports. It also creates the searchable map, as well as a csv that is used to create the searchable table contained on the website using the Python package “csvtotable”. 
 
 There are three separate geographical units profiled alongside each other in the reports to allow for contextualisation/comparison:
 1. An **Electoral Divisions (ED**) is the smallest legally defined administrative areas in the republic of Ireland and their populations vary from 71 to 43,905.
 2. An **Administrative County (AC)** is one of 31 geographical units in Ireland through which Local Government is operated. 
 3. **State** refers to the Republic of Ireland
+
 The “GUID” of a given geographical unit is an identifier that can refer to geographical units across a variety of scales uniquely, including ED, AC and State. 
 
 Dependencies for this project are R (4.3.1 was used in the creation of these profiles), Miktex (an open-source distribution of the LaTeX typesetting system) and Texnic Centre (an open-source Integrated Development Environment  for Latex). 
 
-To use this code, one first needs to clone the repository to a local location. The process as is should run completely from that point. (Note: The “MinimumWorkingExample” folder contains a minimum working example, which – depending on your goals – could be a more appropriate starting point.)
+To use this code, one first needs to clone the repository to a local location and then open "HealthProfileMain.R". The process as is should run completely from that point. (Note: The “MinimumWorkingExample” folder contains a minimum working example, which – depending on your goals – could be a more appropriate starting point.)
 
 **“HealthProfileMain”** is the main driver file. This file will run all the appropriate subfiles and generate the reports without having the user need to open any other files. Initially, it creates a dataframe with ED names, AC names and GUIDs. It then creates a csv of this information that also contains appropriate formatted links to the reports that will be created. It then loops through a list of ED names and finds the associated EDGUID, AC name and AC GUID, which are used to select the appropriate data from given tables/files. It also creates shortened versions of ED and AC names to be used in tables to ensure consistent spacing. For Every ED, AC combination, it sources a map of the ED (see notes for a description of how these maps were created) and loops through all of the associated subfiles to process data and create plots/tables. It then uses the R package Sweave to build the pdf for the Electoral Division. Sweave is a bridge between R and Latex (a language/technology used to create the actual pdf). Finally, it uses RMarkdown to create the HTML report.
 
@@ -49,7 +50,7 @@ There are four main types of data sources used in the project:
 * **JPG maps** of every ED
 
 ## Other notes:
-* When working on this project, line 90 - 91  in “HealthProfileMain” can be commented back in to only run the process on a sample of files to save time. 
+* When working on this project, line 88 - 89  in “HealthProfileMain” can be commented back in to only run the process on a sample of files to save time. 
 * The loop in “HealthProfileMain.R” includes error tracking functionality. If there was an error for an ED, the process skips the ED,  logs the error, the iteration number and the ED GUID and saves the information to “ErrorList.csv” in the output folder.
 * ED maps were created using raw data from OSM and pre-created styles located at https://gitlab.com/champs-libres/public/champs-libres-qgis-osm-style. To create these maps, the raw OSM data was downloaded from geofabrik.de and the shapefiles were copied into the “shp” folder located in of “champs-libres-qgis-osm-style-master”. The champs-libres-qgis-osm-style.qgs file was then opened in QGIS and some minor edits were made. Finally, the QGIS Atlas tool was used to create individual maps for every ED.
-* Headers were added to the index.html page and the table search page manually during the process, as well as some other edits. If trying to recreate, using the Notepad++ plugin “compare” to compare the outputted map and HTML table (via Python package “csvtotable”) to those on the website would be a good starting point. 
+* Headers were added to the index.html page and the table search page manually during the process, as well as some other edits. If trying to recreate, using the Notepad++ plugin “compare” to compare the outputted map and HTML table to those on the website would be a good starting point. 
